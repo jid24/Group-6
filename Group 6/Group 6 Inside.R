958694ae390f0dbe055dc6c6003bbe3727885919
DarcyShu <darcyshu@gmail.com>
2017-03-16 14:36:45 -0400

    Exploring the matrix

---
+++ Group 6/Group 6 Inside.R
@@ -0,0 +1,7 @@
+# The First Project of Group 6
+y1<-matrix(1:9,nrow=3,ncol=3)
+y2<-matrix(11:19,nrow=3,ncol=3)
+yarray<-array(data=c(y1,y2),dim=c(3,3,2))
+yarray[,,2]  
+apply (yarray[,,2],1,sum)
+yarray[,,1]+yarray[,,2]
